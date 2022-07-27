7e55e33371d5e7f20eb7a5a27336f06184fa9c66
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2022-07-27 13:57:35 +0100

    few tweaks

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -5,7 +5,7 @@
             <h1
                 class="text-3xl text-center font-bold my-6 uppercase"
             >
-                Manage Gigs
+                Manage My Gigs
             </h1>
         </header>
 
@@ -18,10 +18,20 @@
                     <td
                         class="px-4 py-8 border-t border-b border-gray-300 text-lg"
                     >
-                        <a href="show.html">
+                        <a href="/listings/{{$listing->id}}">
                             {{$listing->title}}
                         </a>
                     </td>
+
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        
+                            {{$listing->created_at}}
+                        
+                    </td>
+
+
                     <td
                         class="px-4 py-8 border-t border-b border-gray-300 text-lg"
                     >
